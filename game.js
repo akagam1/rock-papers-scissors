a3eb221a1832647ce69efa76eda642835041acab
arjun <arjunputhli00@gmail.com>
2021-12-14 11:18:38 +0530

    fixed small bug with randInt()

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
-function computerPlay() {
-    const choice = ['Rock', 'Paper', 'Scissors'];
-    console.log(choice[randInt()])
+function randInt() {
+    return Math.floor(Math.random()*3);
 }
 
-function randInt() {
-    return Math.floor(Math.random*3);
+function computerPlay() {
+    const choice = ['Rock', 'Paper', 'Scissors'];
+    return choice[randInt()];
 }
 
