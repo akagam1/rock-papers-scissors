0fdc326ba4e8ef923d44ec52ae3569b1b97f91fa
arjun <arjunputhli00@gmail.com>
2021-12-14 12:44:15 +0530

    completed game

--- game.js
+++ game.js
@@ -3,7 +3,65 @@ function randInt() {
 }
 
 function computerPlay() {
-    const choice = ['Rock', 'Paper', 'Scissors'];
+    const choice = ["Rock", "Paper", "Scissors"];
     return choice[randInt()];
 }
+
+function playerSelection() {
+    return prompt("Enter Rock, Paper or Scissors");
+}
+
+function playRound(player,computer) {
+    if (player == "Rock") {
+
+        if (computer == "Rock") {
+            return "It is a tie";
+        }
+
+        if (computer == "Paper") {
+            return `"Computer Wins!" ${computer} beats ${player}`;
+        }
+        if (computer == "Scissors") {
+            return `"You Won"  ${player} beats ${computer}`;
+        }
+    }
+
+    if (player == "Paper") {
+
+        if (computer == "Paper") {
+            return "It is a tie";
+        }
+
+        if (computer == "Scissors") {
+            return `"Computer Wins!" ${computer} beats ${player}`;
+        }
+        if (computer == "Rock") {
+            return `"You Won"  ${player} beats ${computer}`;
+        }
+    }
+
+    if (player == "Scissors") {
+
+        if (computer == "Scissors") {
+            return "It is a tie";
+        }
+
+        if (computer == "Rock") {
+            return `"Computer Wins!" ${computer} beats ${player}`;
+        }
+        if (computer == "Paper") {
+            return `"You Won"  ${player} beats ${computer}`;
+        }
+    }
+}
+
+const games = prompt("How many games do you want to play?");
+for (let i=1; i<=games; i++) {
+    const player = playerSelection();
+    const computer = computerPlay();
+    alert(playRound(player, computer));
+}
+
+
+
 
