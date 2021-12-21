1e4a740df8f148f04cecc8a364a4d5b5a6688980
arjun <arjunputhli00@gmail.com>
2021-12-21 18:29:03 +0530

    added console gameplay feature with player selection and win/loss alert + resetting game

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+let compPoint = 0;
+let userPoint = 0;
+
 function randInt() {
     return Math.floor(Math.random()*3);
 }
@@ -15,9 +18,11 @@ function playRound(player,computer) {
         }
 
         if (computer == "Paper") {
+            compPoint += 1;
             return `"Computer Wins!" ${computer} beats ${player}`;
         }
         if (computer == "Scissors") {
+            userPoint += 1;
             return `"You Won"  ${player} beats ${computer}`;
         }
     }
@@ -29,9 +34,11 @@ function playRound(player,computer) {
         }
 
         if (computer == "Scissors") {
+            compPoint += 1;
             return `"Computer Wins!" ${computer} beats ${player}`;
         }
         if (computer == "Rock") {
+            userPoint += 1;
             return `"You Won"  ${player} beats ${computer}`;
         }
     }
@@ -43,9 +50,11 @@ function playRound(player,computer) {
         }
 
         if (computer == "Rock") {
+            compPoint += 1;
             return `"Computer Wins!" ${computer} beats ${player}`;
         }
         if (computer == "Paper") {
+            userPoint += 1;
             return `"You Won"  ${player} beats ${computer}`;
         }
     }
@@ -54,11 +63,25 @@ function playRound(player,computer) {
 const rockBut = document.getElementById('rockBut');
 const paperBut = document.getElementById('paperBut');
 const scissorBut = document.getElementById('scissorBut');
+const playerPoint = document.getElementsByClassName('playerPoint');
+const computerPoint = document.getElementsByClassName('computerPoint');
 
 rockBut.addEventListener('click', () => clicked('rock'));
 paperBut.addEventListener('click', () => clicked('paper'));
 scissorBut.addEventListener('click', () => clicked('scissors'));
 
 function clicked(playerSelection) {
-    alert(playerSelection);
+    console.log(playRound(playerSelection, computerPlay()));
+    if (userPoint == 5) {
+        alert("You have won the game");
+        compPoint = 0;
+        userPoint = 0;
+    }
+
+    if (compPoint == 5) {
+        alert("Computer won, you lost");
+        compPoint = 0;
+        userPoint = 0;
+
+    }
 }
