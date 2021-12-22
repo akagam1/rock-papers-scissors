c33d7d33966cc42aa88a08297bfb1e8d3e087bcf
arjun <arjunputhli00@gmail.com>
2021-12-22 17:08:29 +0530

    completed ui

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 let compPoint = 0;
 let userPoint = 0;
+let roundWinner = '';
 
 function randInt() {
     return Math.floor(Math.random()*3);
@@ -14,48 +15,49 @@ function playRound(player,computer) {
     if (player == "rock") {
 
         if (computer == "Rock") {
-            return "It is a tie";
+            roundWinner = 'tie';
         }
 
         if (computer == "Paper") {
             compPoint += 1;
-            return `"Computer Wins!" ${computer} beats ${player}`;
+            roundWinner = 'comp';
+            
         }
         if (computer == "Scissors") {
             userPoint += 1;
-            return `"You Won"  ${player} beats ${computer}`;
+            roundWinner = 'player';
         }
     }
 
     if (player == "paper") {
 
         if (computer == "Paper") {
-            return "It is a tie";
+            roundWinner = 'tie';
         }
 
         if (computer == "Scissors") {
             compPoint += 1;
-            return `"Computer Wins!" ${computer} beats ${player}`;
+            roundWinner = 'comp';
         }
         if (computer == "Rock") {
             userPoint += 1;
-            return `"You Won"  ${player} beats ${computer}`;
+            roundWinner = 'player';
         }
     }
 
     if (player == "scissors") {
 
         if (computer == "Scissors") {
-            return "It is a tie";
+            roundWinner = 'tie';
         }
 
         if (computer == "Rock") {
             compPoint += 1;
-            return `"Computer Wins!" ${computer} beats ${player}`;
+            roundWinner = 'comp';
         }
         if (computer == "Paper") {
             userPoint += 1;
-            return `"You Won"  ${player} beats ${computer}`;
+            roundWinner = 'player';
         }
     }
 }
@@ -65,23 +67,36 @@ const paperBut = document.getElementById('paperBut');
 const scissorBut = document.getElementById('scissorBut');
 const playerPoint = document.getElementsByClassName('playerPoint');
 const computerPoint = document.getElementsByClassName('computerPoint');
+const playerScore = document.getElementById('playerScore');
+const computerScore = document.getElementById('computerScore');
 
 rockBut.addEventListener('click', () => clicked('rock'));
 paperBut.addEventListener('click', () => clicked('paper'));
 scissorBut.addEventListener('click', () => clicked('scissors'));
 
 function clicked(playerSelection) {
-    console.log(playRound(playerSelection, computerPlay()));
-    if (userPoint == 5) {
+    playRound(playerSelection, computerPlay());
+    if (roundWinner === 'comp') {
+        computerScore.textContent = `${compPoint}`;
+    }
+    if (roundWinner === 'player') {
+        playerScore.textContent = `${userPoint}`;
+    }
+    if (userPoint === 5) {
         alert("You have won the game");
         compPoint = 0;
         userPoint = 0;
+        playerScore.textContent = `${userPoint}`;
+        computerScore.textContent = `${compPoint}`;
+        roundWinner = '';
     }
 
-    if (compPoint == 5) {
+    if (compPoint === 5) {
         alert("Computer won, you lost");
         compPoint = 0;
         userPoint = 0;
-
+        playerScore.textContent = `${userPoint}`;
+        computerScore.textContent = `${compPoint}`;
+        roundWinner = '';
     }
 }
